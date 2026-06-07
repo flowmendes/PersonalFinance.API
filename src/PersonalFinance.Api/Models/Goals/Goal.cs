bb4bb7456eb6583a2d1fc9ebe540eee36f18f75e
JoaoMendes-A <JoaoMenndesArj@hotmail.com>
2026-06-06 22:04:57 -0300

    fix (Goal.cs): remove comentarios e linhas desnecessárias

--- src/PersonalFinance.Api/Models/Goals/Goal.cs
+++ src/PersonalFinance.Api/Models/Goals/Goal.cs
@@ -3,18 +3,6 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PersonalFinance.Api.Models.Goals;
 
-/// <summary>
-/// Representa uma meta financeira no sistema.
-/// </summary>
-/// <remarks>
-/// Regras de Negócio:
-/// <list type="bullet">
-/// <item><description>O valor alvo (<see cref="TargetAmount"/>) deve ser estritamente maior que zero.</description></item>
-/// <item><description>O tipo (<see cref="Type"/>) determina o impacto da meta no saldo (Economia ou Gasto).</description></item>
-/// <item><description>A data <see cref="CreatedAt"/> deve ser sempre registrada em formato UTC para evitar conflitos de fuso horário.</description></item>
-/// </list>
-/// </remarks>
-
 public class Goal
 {
     [Key]
